ec2f880076baa4f7dafa3e29833fce555c627901
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-07-26 16:40:47 +0200

    capendo errori

---
+++ app/Models/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Slugger;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use Slugger;
+
+    
+    public function category() {
+        return $this->belongsTo('App\Models\Category');
+    }
+}
+
+// Post::getSlug('ciao a tutti');
---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Post;
+use App\Models\Category;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $categories_ids = Category::all()->pluck('id');
+
+        for ($i=0; $i < 150; $i++) { 
+            $post = new Post;
+            $post->title = $faker->words(rand(3, 10), true);
+            // $post->slug = Post::getSlug($post->title);
+            $post->category_id = $faker->randomElement($categories_ids);
+            $post->image = 'https://picsum.photos/id/' . rand(1, 800) . '/200/300';
+            $post->content = $faker->paragraphs(rand(2,10), true);
+            $post->save();
+            // $post->excerpt = $faker->paragrasphs();
+            
+        }
+    }
+}
